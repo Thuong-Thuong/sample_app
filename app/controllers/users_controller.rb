aba59c29fe40b5088d95d70cbba16f01a8e940f1
Thuong-Thuong <ttt.duong@yahoo.fr>
2016-04-06 18:23:25 +0200

    Fini

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
  def show
     @user = User.find(params[:id])
      @titre = @user.nom
-flash[:success] = "Bienvenue dans l'Application Exemple !"
-
   end 
 def index
   @users = User.all
@@ -12,22 +10,11 @@ end
       @user = User.new
       @titre = "Inscription"
    end
-
-	def create
-    @user = User.new(params[:user])
-    if @user.save
-    redirect_to @user
-    flash[:success] = "Bienvenue dans l'Application Exemple !"
-      
-    else
-      @titre = "Inscription"
-      render 'new'
-    end
-  end
 def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to @user
+   	 redirect_to @user
+   	 flash[:success] = "Bienvenue dans l'Application Exemple !"
     else
       @titre = "Inscription"
       render 'new'
