6afd4f65ed72d0d92db67e34cf9dd0db76fd5b6c
Thuong-Thuong <ttt.duong@yahoo.fr>
2016-04-11 01:26:33 +0200

    webrat.log create

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408172738) do
+ActiveRecord::Schema.define(version: 20160410152441) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
+
+  create_table "users1", id: false, force: :cascade do |t|
+    t.integer  "id",                 limit: 4,   default: 0,     null: false
     t.string   "nom",                limit: 255
     t.string   "email",              limit: 255
     t.datetime "created_at"
@@ -22,7 +32,5 @@ ActiveRecord::Schema.define(version: 20160408172738) do
     t.string   "salt",               limit: 255
     t.boolean  "admin",                          default: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
