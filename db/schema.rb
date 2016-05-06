3a1b7894f098ec9fae5dd407cedd1bd1d7a42dea
Thuong-Thuong <ttt.duong@yahoo.fr>
2016-05-06 20:51:39 +0200

    supression columns fprovider

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430143826) do
+ActiveRecord::Schema.define(version: 20160505201332) do
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content",    limit: 255
@@ -38,15 +48,29 @@ ActiveRecord::Schema.define(version: 20160430143826) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "nom",                limit: 255
-    t.string   "email",              limit: 255
+    t.string   "nom",                    limit: 255
+    t.string   "email",                  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password", limit: 255
-    t.string   "salt",               limit: 255
-    t.boolean  "admin",                          default: false
+    t.string   "encrypted_password",     limit: 255
+    t.string   "salt",                   limit: 255
+    t.boolean  "admin",                              default: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "identities", "users"
 end
