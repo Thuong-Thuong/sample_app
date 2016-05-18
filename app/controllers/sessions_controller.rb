b2fafc4bbee4a0bc6151df9ab6e241875cdbf06c
Thuong-Thuong <ttt.duong@yahoo.fr>
2016-05-18 18:48:23 +0200

    avant Friendship

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.authenticate(params[:session][:email],
                              params[:session][:password])
-			#if user.nil?
-				#user = User.from_omniauth(request.env["omniauth.auth"])
-				#session[:user_id] = user.id
-				#session[:user_name] = user.name
-				# sign_in user
-				#redirect_back_or user
-
+			
 				if user.nil?
 					flash.now[:error] = "Combinaison Email/Mot de passe invalide."
 					@titre = "S'identifier"
@@ -24,11 +18,7 @@ class SessionsController < ApplicationController
 					sign_in user
 					redirect_back_or user
 				end
-			#else
-			# Authentifie l'utilisateur et redirige vers sa paged'affichage.
-				#sign_in user
-				#redirect_back_or user
-			#end 
+			
 	end  
 
 	def destroy
