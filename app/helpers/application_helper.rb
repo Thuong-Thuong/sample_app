3625a5f0df8ad2aed73d7fb6ee6848e16649cc5f
Thuong-Thuong DUONG THI <Thuong-Thuong DUONG THI>
2016-05-31 11:11:17 +0200

    files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
 
-  # Retourner un titre basé sur la page.
-  def titre
-    base_titre = "Simple App du Tutoriel Ruby on Rails"
-    if @titre.nil?
-      base_titre
-    else
-      "#{base_titre} | #{@titre}"
-    end
-  end
-  def logo
-    image_tag("logo.png", :alt => "Application Exemple", :class => "round")
-  end
-  
+	#Retourner un titre basé sur la page.
+	def titre
+		base_titre = "Simple App du Tutoriel Ruby on Rails"
+		if @titre.nil?
+			base_titre
+		else
+			"#{base_titre} | #{@titre}"
+		end
+	end
+	def logo
+		image_tag("logo.png", :alt => "Application Exemple", :class => "round")
+	end
+ 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
-# Returns the Gravatar for the given user.
-  def gravatar_pour(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.nom,
+	# Returns the Gravatar for the given user.
+	def gravatar_pour(user, options = { :size => 50 })
+		gravatar_image_tag(user.email.downcase, :alt => user.nom,
                                          :class => 'gravatar',
                                          :gravatar => options)
-  end
+	end
 end
