0c377c1646913d073f6901b18123fa24b49cb02d
Thuong-Thuong DUONG THI <Thuong-Thuong DUONG THI>
2016-07-02 16:26:18 +0200

    ok

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -5,6 +5,8 @@ module FriendshipsHelper
 			$status=5
 			current_user.friendships.find_by_receiver_id(@user)
 		end
-		
+	end
+	def friendship_break
+		current_user.friendships.find_by_receiver_id_and_sender_id(@user,current_user.id)
 	end
 end
--- app/models/commentaire.rb
+++ app/models/commentaire.rb
@@ -1,12 +1,12 @@
 class Commentaire < ActiveRecord::Base
 	attr_accessible :commentaire 
 	belongs_to :evenement
-    belongs_to :user
-    has_many :reponses, :dependent => :destroy 
+	belongs_to :user
+	has_many :reponses, :dependent => :destroy 
 	validates :commentaire, :presence => true
 	
 	def init(commentateur, evenement)
-    		self.commentateur_id  = commentateur
-    		self.evenement_id = evenement
+		self.commentateur_id  = commentateur
+		self.evenement_id = evenement
 	end
  end
