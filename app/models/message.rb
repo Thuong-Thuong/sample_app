cb4302457ea68af0a8d6cc2c5507d36430ddb4b2
Thuong-Thuong DUONG THI <Thuong-Thuong DUONG THI>
2016-07-07 19:27:32 +0200

    ko

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,6 @@ class Message < ActiveRecord::Base
 	attr_accessible :message
 	belongs_to :user, :class_name => "User"
 	validates :message, :presence => true
-	has_many :reverse_messages, :foreign_key => "sender_id",
-                                   :class_name => "Message"
 	default_scope { order(created_at: :desc) }
 	def init(sender,receiver)
 		self.sender_id  = sender
