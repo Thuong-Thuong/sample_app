6343d9d449345ddcab42f7884767c520a712bfe6
Thuong-Thuong DUONG THI <Thuong-Thuong DUONG THI>
2016-07-09 18:20:47 +0200

    ok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,18 @@ class MessagesController < ApplicationController
 	def create
 		@message = Message.new
 		@message = current_user.messages.build(params[:message])
-		@message.init(current_user.id,$user)
+		if !($irep == 1 ) 
+			@message.init(current_user.id,$user)
+		else
+			@message.init(current_user.id,$receiver_id)
+		end
 		if @message.save
-			flash[:success] = "Message created!" 
+			flash[:success] = "Message created! #{$irep}" 
+			if !($irep == 1 )
 			redirect_to messages_path
+			else
+			redirect_to messages_path
+			end
 		else
 			render 'show'
 		end
@@ -21,9 +29,20 @@ class MessagesController < ApplicationController
 	def show
 		@message = current_user.messages.build(params[:message])
 		if current_user.id == $user
-			@feed_item_messages = Message.all.where('receiver_id = ?', current_user.id)
+			if !($irep == 1)
+				@feed_item_messages = Message.all.where('receiver_id = ?', current_user.id)
+			else 
+				@feed_item_messages = Message.all.where('sender_id = ? AND receiver_id = ?', current_user.id,$receiver_id)
+			end
 		else
-			@feed_item_messages = Message.all.where('sender_id = ?', current_user.id)
+			if !$irep == 1
+				@feed_item_messages = Message.all.where('sender_id = ?', current_user.id)
+			else 
+				@feed_item_messages = Message.all.where('sender_id = ? AND receiver_id = ?', current_user.id, $receiver_id )
+				if @feed_item_message.nil?
+					@feed_item_messages = Message.all.where('sender_id = ? AND receiver_id = ?', current_user.id, $user )
+				end
+			end
 		end
 		if !@feed_item_messages.nil?
 			@feed_item_messages= @feed_item_messages.paginate(:page => params[:page])
@@ -32,10 +51,19 @@ class MessagesController < ApplicationController
 	
 	def edit
 		@message = current_user.messages.build(params[:message])
+		if $irep == 1
+			$message = Message.find(params[:id])
+			if $message.nil?
+				$message = @message
+			end
+			$receiver_id = $message.sender_id
+		else
+			$receiver_id = $message.receiver_id
+		end
 		if current_user.id == $user
 			@feed_item_messages = Message.all.where('receiver_id = ?', current_user.id)
 		else
-			@feed_item_messages = Message.all.where('sender_id = ?', current_user.id)
+			@feed_item_messages = Message.all.where('sender_id = ? AND receiver_id = ?', current_user.id,$user)
 		end
 		if !@feed_item_messages.nil?
 			@feed_item_messages= @feed_item_messages.paginate(:page => params[:page])
@@ -54,8 +82,8 @@ class MessagesController < ApplicationController
 	
 	def destroy
 	    @message.destroy
-		flash[:success] = "Message supprime!"
-		redirect_to messages_path
+		flash[:success] = "Message supprime !"
+		redirect_to(:back)
 	end
 
 	private
