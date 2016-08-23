2381dacff3a5e4141d09e8eea452cf2178215884
Thuong-Thuong DUONG THI <Thuong-Thuong DUONG THI>
2016-08-24 00:51:52 +0200

    ok

--- db/migrate/20160823220717_changecolumnfrommessages.rb
+++ db/migrate/20160823220717_changecolumnfrommessages.rb
@@ -1,8 +1,4 @@
 class Changecolumnfrommessages < ActiveRecord::Migration
   def change
-      t.change :i_sup, 'integer USING CAST(i_sup AS integer)',
- :default => 0
-	 t.change :i_sup_rec, 'integer USING CAST(i_sup_rec AS integer)', :default => 0
-    end
   end
 end
